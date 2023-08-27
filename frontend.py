994a79d663e0c2be0fb2ae3bf21be920f47804a0
safimuhammad <safiwahid38@gmail.com>
2023-08-27 23:12:32 +0500

    finalizing

---
+++ frontend.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from PIL import Image
+from models import weapon_detection
+from main import llma_control
+import os
+import uuid
+import time
+import re
+
+
+def save_uploaded_file(uploaded_file):
+    # Generate a unique filename
+    unique_filename = str(uuid.uuid4()) + "_" + uploaded_file.name
+
+    # Save the file in the "uploads" folder
+    save_path = os.path.join("uploads", unique_filename)    
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    return unique_filename
+
+def main():
+    st.title("DefenceIQ")
+    st.write("Upload an image to decide the course of action")
+
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    st.sidebar.title("Strategy To Act")
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        unique_filename = save_uploaded_file(uploaded_file)
+      
+        
+        with st.spinner("Analyzing Potential Suspicion..."):
+            status= weapon_detection(image=f"uploads/{unique_filename}")
+            st.header(status)
+
+    
+    if uploaded_file:    
+        st.sidebar.header('Verifying Detections')
+        with st.spinner("Preparing Course of action..."):
+            if status is None:
+                st.write("No anomaly Detected")
+            else:
+                action_plan= llma_control(status)
+                print(action_plan)
+                st.sidebar.write(action_plan)
+
+                steps = re.split(r'\d+\.', action_plan)  # Split based on numbers followed by a period
+
+                st.warning(steps[0].strip())  # Display the introduction
+
+                for step in steps[1:]:
+                    time.sleep(3)
+                    st.warning(step.strip())  # Display each step
+                    time.sleep(3)  # Pause for a few seconds between steps
+                st.success("Alert sequence completed. Remember to ensure everyone's safety and stay vigilant.")
+
+                
+    
+if __name__ == '__main__':
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+    main()
--- models.py
+++ models.py
@@ -1,40 +1,28 @@
-##############################################################################
-# In this section, we set the user authentication, app ID, workflow ID, and  
-# image URL. Change these strings to run your own example.
-##############################################################################
-
-USER_ID = 'na00lqzr91iu'
-# Your PAT (Personal Access Token) can be found in the portal under Authentification
-PAT = 'b5ffeaa73b19476d94e8c481107583e7'
-APP_ID = 'testproject'
-# Change these to make your own predictions
-WORKFLOW_ID = 'workflow-d4ebf7'
-IMAGE_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBv-SC2ANPFUaxUx6zGTeBCfJPjNUaf3Xfcg&usqp=CAUU'
-
-
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 import time
-import json 
+import json
+from dotenv import load_dotenv  
+import os
 
 
-
-def weapon_detection():
-    USER_ID = 'na00lqzr91iu'
-    # Your PAT (Personal Access Token) can be found in the portal under Authentification
-    PAT = 'b5ffeaa73b19476d94e8c481107583e7'
-    APP_ID = 'testproject'
-    # Change these to make your own predictions
-    WORKFLOW_ID = 'workflow-d4ebf7'
-    IMAGE_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBv-SC2ANPFUaxUx6zGTeBCfJPjNUaf3Xfcg&usqp=CAUU'
-
+def weapon_detection(image):
+    load_dotenv()
+    USER_ID = os.getenv("USER_ID")
+    PAT = os.getenv("PAT")
+    WORKFLOW_ID = os.getenv("WORKFLOW_ID")
+    APP_ID = os.getenv("APP_ID")
+    IMAGE_FILE_LOCATION = image
     channel = ClarifaiChannel.get_grpc_channel()
     stub = service_pb2_grpc.V2Stub(channel)
 
     metadata = (('authorization', 'Key ' + PAT),)
 
     userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID) # The userDataObject is required when using a PAT
+
+    with open(IMAGE_FILE_LOCATION, "rb") as f:
+        file_bytes = f.read()
 
     post_workflow_results_response = stub.PostWorkflowResults(
         service_pb2.PostWorkflowResultsRequest(
@@ -44,7 +32,7 @@ def weapon_detection():
                 resources_pb2.Input(
                     data=resources_pb2.Data(
                         image=resources_pb2.Image(
-                            url=IMAGE_URL
+                            base64=file_bytes
                         )
                     )
                 )
@@ -64,7 +52,6 @@ def weapon_detection():
             if concept.value > 0.60:
                 detections.append(concept.name)
                 print(f"Name: {concept.name} , Value: {concept.value}")
-                # print(f"{concept.name} detected in the building Entrance.")
-    return f"{detections} detected in the building Entrance." if len(detections) >0 else None
+              
+    return f"{detections} detected in the building." if len(detections) >0 else None
 
-print(weapon_detection())
